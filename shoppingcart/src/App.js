320e6e818e1830903e13a4e7897c0567ea0be2ad
Vimleshkumari <vimlesh3012@example.com>
2021-09-30 10:09:34 +0530

    "list done

---
+++ shoppingcart/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Home/>
+      <div>Footer</div>
+      </div>
+  );
+}
+
+export default App;
